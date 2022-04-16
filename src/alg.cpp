246d66ad5c585faaf0dff9b93d64dbe801f42fec
alyonatokareva <99283237+alyonatokareva@users.noreply.github.com>
2022-04-16 17:28:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
-
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+// поместить сюда реализацию алгоритма
+return 0; // если ничего не найдено
+int left = 0, right = size - 1, a = 0;
+while (left < right) {
+int center = left + (right - left) / 2;
+if (arr[center] == value) {
+left = center;
+while (arr[left] == value) {
+left--;
+}
+while (arr[right] != value) {
+right--;
+}
+while (left < right) {
+a++;
+left++;
+}
+} else if (arr[center] > value) {
+right = center;
+} else {
+left = center + 1;
+}
+}
+return a;
 }
